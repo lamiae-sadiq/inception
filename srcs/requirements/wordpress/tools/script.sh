2f2c182fc43ff3239ae9db2956727c8e637600a6
Weirdo <slamiae66@gmail.com>
2024-05-03 18:41:42 +0100

    mariadb...

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,3 +1,15 @@
-#bin/bash
+cd /var/www/html/wordpress
 
-echo " hello world "
+wp core download --path="/var/www/html/wordpress" --allow-root
+
+wp config create --path="/var/www/html/wordpress" --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --dbprefix=wp_
+
+wp core install --path="/var/www/html/wordpress" --allow-root --url=$DOMAIN_NAME --title="$WP_TITLE" --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL
+
+wp user create --path="/var/www/html/wordpress" --allow-root $WP_USR $WP_EMAIL --user_pass=$WP_USER_PWD
+
+chown www-data:www-data /var/www/html/wordpress/wp-content/uploads -R
+
+mkdir -p /run/php/
+
+php-fpm7.4 -F
