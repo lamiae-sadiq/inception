a09fba86d9a2ec1d074b3e94e53e9a34bd899f2a
lsadiq <slamiae66@gmail.com>
2024-05-12 20:51:18 +0100

    DaTa_Base.....-_-

--- srcs/requirements/mariadb/tools/maria.sh
+++ srcs/requirements/mariadb/tools/maria.sh
@@ -9,10 +9,10 @@ mysqld_safe &
 
 
 
-until mariadb -u root -h localhost --password="${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"; do
-  echo "Database is not up yet. Waiting..."
-  sleep 0.2 # wait for 5 seconds before check again
-done
+# until mariadb -u root -h localhost --password="${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"; do
+#   echo "Database is not up yet. Waiting..."
+#   sleep 0.2 # wait for 5 seconds before check again
+# done
 
 
 
@@ -20,13 +20,13 @@ done
 
 # create a database if it's not exist
 
+echo 1
 echo " create a database if it's not exist"
 mariadb -u root -h localhost --password="${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
 CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;"
-echo 1
 # echo " create a database if it's not exist"
 # mariadb -u root -h localhost --password="${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -5,12 +5,12 @@ cd /var/www/
 #   echo "Database is not up yet. Waiting..."
 #   sleep 0.2 # wait for 5 seconds before check again
 # done
-
+sleep 3;
 
 wget https://wordpress.org/latest.tar.gz && tar xzvf latest.tar.gz && rm -rf latest.tar.gz 
 sed -i 's|listen = /run/php/php7.4-fpm.sock|listen = 0.0.0.0:9000|g' /etc/php/7.4/fpm/pool.d/www.conf
 
-chown -R www-data:www-data /var/www/wordpress/
+chown -R www-data:www-data /var/www/html/wordpress/
 
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
@@ -19,17 +19,17 @@ chmod +x wp-cli.phar
 
 mv wp-cli.phar /usr/bin/wp-cli
 
-# cd /var/www/wordpress
+# cd /var/www/html/wordpress
 
-# wp core download --path="/var/www/wordpress" --allow-root
+# wp core download --path="/var/www/html/wordpress" --allow-root
 
-# wp config create --path="/var/www/wordpress" --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --dbprefix=wp_
+# wp config create --path="/var/www/html/wordpress" --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --dbprefix=wp_
 
-# wp core install --path="/var/www/wordpress" --allow-root --url=$DOMAIN_NAME --title="$WP_TITLE" --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL
+# wp core install --path="/var/www/html/wordpress" --allow-root --url=$DOMAIN_NAME --title="$WP_TITLE" --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL
 
-# wp user create --path="/var/www/wordpress" --allow-root $WP_USR $WP_EMAIL --user_pass=$WP_USER_PWD
+# wp user create --path="/var/www/html/wordpress" --allow-root $WP_USR $WP_EMAIL --user_pass=$WP_USER_PWD
 
-# chown www-data:www-data /var/www/wordpress/ -R
+# chown www-data:www-data /var/www/html/wordpress/ -R
 
 mkdir -p /run/php/
 
