b93cf97b93413136bb3d4453f3966636e7bb905e
lsadiq <slamiae66@gmail.com>
2024-05-18 18:53:59 +0100

    Mandatory Done .

--- srcs/requirements/mariadb/tools/maria.sh
+++ srcs/requirements/mariadb/tools/maria.sh
@@ -22,25 +22,25 @@ done
 
 echo 1
 echo " create a database if it's not exist"
-mariadb -u root -h localhost --password="${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
-CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-FLUSH PRIVILEGES;"
-# echo " create a database if it's not exist"
-# mariadb -u root -h localhost --password="${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+# mariadb -u root -h localhost --password="${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+# CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+# GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%';
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+# FLUSH PRIVILEGES;"
+#echo " create a database if it's not exist"
+mariadb -u root -h localhost --password="${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 
-# echo "create the user if it's not exists (adding localhost so the user can only connect from the localhost)"
-# mariadb -u root -h localhost --password="${MYSQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+#echo "create the user if it's not exists (adding localhost so the user can only connect from the localhost)"
+mariadb -u root -h localhost --password="${MYSQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
-# echo "give the user all the privilages of the database"
-# mariadb -u root -h localhost --password="${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%';"
+#echo "give the user all the privilages of the database"
+mariadb -u root -h localhost --password="${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%';"
 
-# echo "modify the user: root connecting form: localhsot old password and replace it with the new password"
-# mariadb -u root -h localhost --password="${MYSQL_ROOT_PASSWORD}" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+#echo "modify the user: root connecting form: localhsot old password and replace it with the new password"
+mariadb -u root -h localhost --password="${MYSQL_ROOT_PASSWORD}" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 
-# echo "reload the grant tables memory for the changes to take effect immediately"
-# mariadb -u root -h localhost --password="${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+#echo "reload the grant tables memory for the changes to take effect immediately"
+mariadb -u root -h localhost --password="${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
 
 # echo "shutdown the sql server"
 # mysqladmin -u root --password="${MYSQL_ROOT_PASSWORD}" shutdown
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,15 +1,23 @@
-cd /var/www/
+mkdir -p /var/www/wordpress
+cd /var/www/wordpress
 
 
 until mariadb -u "$MYSQL_USER" -h mariadb --password="${MYSQL_PASSWORD}" -e "show databases;"; do
   echo "Database is not up yet. Waiting..."
   sleep 0.2 # wait for 5 seconds before check again
 done
-# sleep 3;
-
-wget https://wordpress.org/latest.tar.gz && tar xzvf latest.tar.gz && rm -rf latest.tar.gz 
 sed -i 's|listen = /run/php/php7.4-fpm.sock|listen = 0.0.0.0:9000|g' /etc/php/7.4/fpm/pool.d/www.conf
 
+
+wp core download  --allow-root
+
+wp config create --allow-root  --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb 
+
+wp core install --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL  --allow-root --url=$DOMAIN_NAME
+
+wp user create --user=$WP_USER --user-email=$WP_USER_EMAIL --role=author --user_pass=$WP_USER_PWD --allow-root
+
+chown -R www-data:www-data /var/www/wordpress
 
 mkdir -p /run/php/
 
