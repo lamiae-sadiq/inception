0ae5a5eb11579d026e0ce853d745ad918b56bb37
lsadiq <slamiae66@gmail.com>
2024-06-01 21:01:28 +0100

    Push

--- srcs/requirements/bonus/monitorix/tools/monitorix.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-service monitorix restart
-#restart the server to work properly
-
-nc -l -p 1212 #makes the container on
-# -l : Puts 'nc' in listening mode, waiting for incoming connections.
-# -p : Specifies the source port 'nc' should use, subject to privilege restrictions and availability.
